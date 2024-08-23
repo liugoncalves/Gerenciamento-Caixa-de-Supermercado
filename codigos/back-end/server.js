2e14ce6dc522000f21786e3258ddb60b28439153
Leo Gonçalves <110125502+liugoncalves@users.noreply.github.com>
2024-08-23 10:11:32 -0300

    [refactor] alterando import no server.js

--- codigos/back-end/server.js
+++ codigos/back-end/server.js
@@ -1,26 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-<<<<<<< HEAD
-=======
-import funcionarioRouter from './src/back-end/routes/funcionario-route.js';
-import produtoRouter from './src/back-end/routes/produto-route.js';
-import clienteRouter from './src/back-end/routes/cliente-route.js';
-import enderecoRouter from './src/back-end/routes/endereco-route.js';
-import vendaRouter from './src/back-end/routes/venda-route.js';
->>>>>>> development
+import funcionarioRouter from './codigos/back-end/routes/funcionario-route.js';
+import produtoRouter from './codigos/back-end/routes/produto-route.js';
+import clienteRouter from './codigos/back-end/routes/cliente-route.js';
+import enderecoRouter from './codigos/back-end/routes/endereco-route.js';
+import vendaRouter from './codigos/back-end/routes/venda-route.js';
 
 const port = 3002;
 const app = express();
 
 app.use(bodyParser.json());
-<<<<<<< HEAD
-=======
 app.use('/funcionarios', funcionarioRouter);
 app.use('/produtos', produtoRouter);
 app.use('/clientes', clienteRouter);
 app.use('/enderecos', enderecoRouter);
 app.use('/vendas', vendaRouter);
->>>>>>> development
 
 // Adicionar uma rota básica
 app.get('/', (req, res) => {
