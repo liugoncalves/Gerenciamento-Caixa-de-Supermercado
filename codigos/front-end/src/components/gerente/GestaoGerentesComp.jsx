70cb8ac6de5610ae1502adc2dd7a8f2294eee7fb
MaiconSouzah <maicon.souza@estudante.ufla.br>
2024-08-24 19:48:40 -0300

    [feat]: Tela de gestão do gerente

--- codigos/front-end/src/components/gerente/GestaoGerentesComp.jsx
+++ codigos/front-end/src/components/gerente/GestaoGerentesComp.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../styles/gerente/GestaoGerenteComp.css';
 
+// Supondo que você tenha os ícones nas seguintes pastas:
+import funcionarioIcon from '../../assets/images/funcionarios.png';
+import clienteIcon from '../../assets/images/clientes.png';
+import produtoIcon from '../../assets/images/produtos.png';
+import vendasIcon from '../../assets/images/vendas.png';
+import comprasIcon from '../../assets/images/compras.png';
+
 const GestaoGerente = () => {
     const navigate = useNavigate();
 
@@ -11,15 +18,27 @@ const GestaoGerente = () => {
 
     return (
         <div className="gestao-container">
-            <div className="header">
-                <h1>Gestão Gerente</h1>
-            </div>
             <div className="button-container">
-                <button onClick={() => handleButtonClick('/gerenciar-funcionarios')}>Gerenciar Funcionários</button>
-                <button onClick={() => handleButtonClick('/gerenciar-clientes')}>Gerenciar Clientes</button>
-                <button onClick={() => handleButtonClick('/gerenciar-produtos')}>Gerenciar Produtos</button>
-                <button onClick={() => handleButtonClick('/exibir-vendas')}>Exibir Vendas</button>
-                <button onClick={() => handleButtonClick('/exibir-compras')}>Exibir Compras do Cliente</button>
+                <button onClick={() => handleButtonClick('/gerenciar-funcionarios')}>
+                    <img src={funcionarioIcon} alt="Gerenciar Funcionários" />
+                    <span>Gerenciar Funcionários</span>
+                </button>
+                <button onClick={() => handleButtonClick('/gerenciar-clientes')}>
+                    <img src={clienteIcon} alt="Gerenciar Clientes" />
+                    <span>Gerenciar Clientes</span>
+                </button>
+                <button onClick={() => handleButtonClick('/gerenciar-produtos')}>
+                    <img src={produtoIcon} alt="Gerenciar Produtos" />
+                    <span>Gerenciar Produtos</span>
+                </button>
+                <button onClick={() => handleButtonClick('/exibir-vendas')}>
+                    <img src={vendasIcon} alt="Exibir Vendas" />
+                    <span>Exibir Vendas</span>
+                </button>
+                <button onClick={() => handleButtonClick('/exibir-compras')}>
+                    <img src={comprasIcon} alt="Exibir Compras" />
+                    <span>Exibir Compras dos Clientes</span>
+                </button>
             </div>
         </div>
     );
--- codigos/front-end/src/pages/gerente/gerente-gestao.jsx
+++ codigos/front-end/src/pages/gerente/gerente-gestao.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import GestaoGerente from '../../components/gerente/GestaoGerentesComp' // Certifique-se de que o caminho esteja correto
+import GestaoGerente from '../../components/gerente/GestaoGerentesComp'; // Certifique-se de que o caminho esteja correto
 import '../../styles/gerente/GestaoGerente-pag.css'; // Para os estilos da página
 import LogoutButton from '../../components/geral/LogoutButton';
+
 const PaginaGestao = () => {
     return (
         <div className="pagina-gestao-container">
             <LogoutButton />
             <header className="pagina-header">
-                <h1>Painel de Gestão</h1>
+                <h1>Gestão - Gerente</h1>  
             </header>
             <main className="pagina-main">
                 <GestaoGerente />
